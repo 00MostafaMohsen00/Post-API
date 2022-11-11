5ba5d86c45f5f56660fc9d4f2a4596c9daae57c8
00MostafaMohsen00 <geye096@gmail.com>
2022-11-12 01:07:55 +0200

    api post

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ Route::group(['middleware'=>'jwt.verify'],function(){
 
 
 
-Route::group(['middleware'=>"api","prefix"=>"auth"],function($router){
+Route::group(["prefix"=>"auth"],function($router){
     Route::post('/login',[AuthController::class,"login"]);
     Route::post('/register',[AuthController::class,"register"]);
     Route::post('/logout',[AuthController::class,"logout"]);
